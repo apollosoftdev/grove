309e7ff53b5938c8a34bc5585a152932c11ec9df
Felix2665 <Felix2665@outlook.com>
2026-06-29 21:00:51 -0600

    feat: delete old migrateion files

--- prisma/migrations/20260628103832_quantity/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `CartItem` ADD COLUMN `quantity` INTEGER NOT NULL DEFAULT 1;
--- prisma/migrations/20260630030008_products_in_cart/migration.sql
+++ prisma/migrations/20260630030008_products_in_cart/migration.sql
@@ -1,18 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `FavouriteProducts` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE `FavouriteProducts` DROP FOREIGN KEY `FavouriteProducts_imageId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `FavouriteProducts` DROP FOREIGN KEY `FavouriteProducts_userId_fkey`;
-
--- DropTable
-DROP TABLE `FavouriteProducts`;
-
 -- CreateTable
 CREATE TABLE `Carts` (
     `id` VARCHAR(191) NOT NULL,
@@ -29,16 +14,17 @@ CREATE TABLE `CartItem` (
     `id` VARCHAR(191) NOT NULL,
     `cartId` VARCHAR(191) NOT NULL,
     `productId` VARCHAR(191) NOT NULL,
+    `quantity` INTEGER NOT NULL DEFAULT 1,
 
     UNIQUE INDEX `CartItem_cartId_productId_key`(`cartId`, `productId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Carts` ADD CONSTRAINT `Carts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Carts` ADD CONSTRAINT `Carts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `CartItem` ADD CONSTRAINT `CartItem_cartId_fkey` FOREIGN KEY (`cartId`) REFERENCES `Carts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `CartItem` ADD CONSTRAINT `CartItem_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `CartItem` ADD CONSTRAINT `CartItem_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
